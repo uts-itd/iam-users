0f39d7646444db4e08adaff65b2bc88ed2762e7d
Dan <me.daniel.meier@gmail.com>
2020-11-02 18:53:42 +0100

    final fix

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ resource "aws_iam_user" "user" {
 resource "aws_iam_access_key" "credentials" {
   for_each      = var.users
   user          = each.key
+  depends_on = [
+    aws_iam_user.user,
+  ]
 }
 
 resource "aws_secretsmanager_secret" "credentials" {
@@ -24,5 +27,8 @@ resource "aws_secretsmanager_secret_version" "credentials" {
   for_each      = var.users
 
   secret_id     = aws_secretsmanager_secret.credentials[each.key].id
-	secret_string = jsonencode({"AccessKey" = aws_iam_access_key.credentials.*.id, "SecretAccessKey" = aws_iam_access_key.credentials.*.secret})
+	secret_string = jsonencode({"AccessKey" = aws_iam_access_key.credentials[each.key].id, "SecretAccessKey" = aws_iam_access_key.credentials[each.key].secret})
+	depends_on = [
+   aws_secretsmanager_secret.credentials,
+  ]
 }
