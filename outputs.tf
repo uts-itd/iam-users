1f4a4c39c295aba2d15f902420a9525330eb512b
bruc31984 <52766521+bruc31984@users.noreply.github.com>
2022-07-13 10:48:00 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,13 @@ output "user_ids" {
   }
 }
 
+output "user_name" {
+  value = {
+    for users in aws_iam_user.user:
+    users.name => users.name
+  }
+}
+  
 output "user_arns" {
   value = {
     for users in aws_iam_user.user:
