6e3e2bc9147162bcf1a650567871e76fb6fadec2
bruc31984 <52766521+bruc31984@users.noreply.github.com>
2022-07-14 12:02:02 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,11 @@
 output "user_ids" {
-  value = {
-    for users in aws_iam_user.user:
-    users.name => users.unique_id
-  }
+  value       = values(aws_iam_user.user)[*].unique_id
 }
 
 output "user_name" {
   value       = values(aws_iam_user.user)[*].name
-  #value = {
-    #for users in aws_iam_user.user:
-    #users.name => users.name
-  #}
 }
   
 output "user_arns" {
-  value = {
-    for users in aws_iam_user.user:
-    users.name => users.arn
-  }
+  value       = values(aws_iam_user.user)[*].arn
 }
